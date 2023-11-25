c6ec2df6a3524e07922554cec7562676aaab34ff
Mareczekol <mareczek2024@gmail.com>
2023-11-25 05:03:14 +0100

    Add send email button and func

--- main.py
+++ main.py
@@ -1,36 +1,53 @@
+import random
+import smtplib
+from email.mime.text import MIMEText
+import customtkinter
 import requests
 from bs4 import BeautifulSoup
-import customtkinter
 from customtkinter import *
-import random
-from PIL import ImageTk, Image
+from email.mime.multipart import MIMEMultipart
+
 
 set_appearance_mode("Dark")
 set_default_color_theme("blue")
-appWidth, appHeight = 1000, 1000
+appWidth, appHeight = 1000, 1300
+
+
+def get_recipe_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    skladniki = soup.find("div", class_="group-skladniki "
+                                        "field-group-div")
+    przygotowanie = soup.find("div",
+                              class_="field field-name-field-przygotowanie "
+                                     "field-type-text-long field-label-"
+                                     "above")
+    scraped_skladniki = ' '.join(
+            element.get_text() for element in skladniki)
+    scraped_przygotowanie = ' '.join(
+            element.get_text() for element in przygotowanie)
+    return scraped_skladniki, scraped_przygotowanie
 
 
 class GUI(customtkinter.CTk):
     """
-    A class to design the graphic interface
-    """
+       A class to design the graphic interface:
+        - buttons
+        - labels
+        - view tabs
+       """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.title("Generator przepisów kulinarnych")
+        self.title("Food App")
         self.geometry(f"{appWidth}x{appHeight}")
 
-        # img = ImageTk.PhotoImage(Image.open(""))
-        # l1 = CTkLabel(master=app, image=img)
-        # l1.pack()
-        #
-        # add View Tab
         self.viewTab = CTkTabview(self)
         self.viewTab.grid(padx=200, pady=10, sticky="e")
         self.viewTab.add("Śniadanie")
         self.viewTab.add("Obiad")
         self.viewTab.add("Kolacja")
 
-        # add Tab. 1 - śniadanie
+        # Tab. 1 - śniadanie
         self.platki_btn = CTkButton(self.viewTab.tab("Śniadanie"),
                                     text="Płatki", command=self.platki_btn)
         self.platki_btn.grid(padx=80, pady=20, sticky="sw")
@@ -48,7 +65,7 @@ class GUI(customtkinter.CTk):
                                     text="Placki", command=self.placki_btn)
         self.placki_btn.grid(padx=80, pady=35, sticky="sw")
 
-        # add Tab 2 - obiad
+        # Tab 2 - obiad
         self.losos_btn = CTkButton(self.viewTab.tab("Obiad"),
                                    text="Łosoś", command=self.losos_btn)
         self.losos_btn.grid(padx=80, pady=20, sticky="sw")
@@ -65,7 +82,7 @@ class GUI(customtkinter.CTk):
                                   text="Wieprzowina", command=self.pork_btn)
         self.pork_btn.grid(padx=80, pady=35, sticky="sw")
 
-        # add Tab. 3 - kolacja
+        # Tab. 3 - kolacja
         self.light_btn = CTkButton(self.viewTab.tab("Kolacja"),
                                    text="Lekka kolacja", command=self.light_btn)
         self.light_btn.grid(padx=80, pady=20, sticky="sw")
@@ -75,53 +92,29 @@ class GUI(customtkinter.CTk):
                                   command=self.warm_btn)
         self.warm_btn.grid(padx=80, pady=25, sticky="sw")
 
-        # add recipe text box
-        self.recipe_text = CTkTextbox(self, width=800, height=650)
+        self.recipe_text = CTkTextbox(self, width=800, height=800)
         self.recipe_text.grid(row=7, column=0, columnspan=4, padx=20, pady=20,
                               sticky="nsew")
 
-    # define button commands
+        # email button
+        self.email_btn = CTkButton(self, text="Wyślij na email",
+                                   command=self.email_btn)
+        self.email_btn.grid(row=8, column=0, padx=30, pady=30,
+                            sticky="nsew")
 
     def platki_btn(self):
-        urls = ["https://www.kwestiasmaku.com/przepis/musli-z-jablkiem",
-                "https://www.kwestiasmaku.com/przepis/owsianka-z-chia-i-"
-                "granatem",
-                "https://www.kwestiasmaku.com/dania_dla_dwojga/sniadania/chia/"
-                "przepis."
-                "html",
-                ]
+        urls = [
+            "https://www.kwestiasmaku.com/przepis/musli-z-jablkiem",
+            "https://www.kwestiasmaku.com/przepis/owsianka-z-chia-i-granatem",
+            "https://www.kwestiasmaku.com/dania_dla_dwojga/sniadania/chia/"
+            "przepis.html"
+        ]
         url = random.choice(urls)
-        response = requests.get(url)
-        # print(response.content)  # Debug: Print the response content
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
-
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
-        # for another website
-        # skladniki_1 = soup.find(soup.find("div", class_="recipe--"
-        #                                                 "ingredients--list"))
-        # przygotowanie_1 = soup.find("div", class_="recipeStepsComponent")
-        # scraped_1 = ' '.join(element.get_text() for element in
-        # przygotowanie_1)
-        # self.recipe_text.insert("1.0", scraped_1)
-        # scraped_text_1 = ' '.join(element.get_text() for element in
-        # skladniki_1)
-        # self.recipe_text.insert("1.0", scraped_text_1)
-
-        # debug
-        # print(scraped_text)
-        # print(scraped)
-
-    # add buttons functionality
     def jajka_btn(self):
         urls = [
             "https://www.kwestiasmaku.com/przepis/pasztet-jajeczny",
@@ -129,19 +122,10 @@ class GUI(customtkinter.CTk):
             "https://www.kwestiasmaku.com/przepis/tortille-z-cukinii"
         ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", str.format(scraped))
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-
-        self.recipe_text.insert("1.0", str.format(scraped_text))
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
     def jaglanka_btn(self):
         urls = [
@@ -152,18 +136,10 @@ class GUI(customtkinter.CTk):
             "z_mlekiem_kokosowym_zurawina/przepis.html"
         ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
     def placki_btn(self):
         urls = [
@@ -175,19 +151,10 @@ class GUI(customtkinter.CTk):
                 "jablkiem"
         ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
-
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
     def kurczak_btn(self):
         urls = [
@@ -198,18 +165,10 @@ class GUI(customtkinter.CTk):
 
         ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
     def losos_btn(self):
         urls = [
@@ -222,18 +181,10 @@ class GUI(customtkinter.CTk):
 
         ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
     def beef_btn(self):
         urls = [
@@ -245,18 +196,10 @@ class GUI(customtkinter.CTk):
 
         ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
     def pork_btn(self):
         urls = [
@@ -268,18 +211,10 @@ class GUI(customtkinter.CTk):
 
         ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
     def light_btn(self):
         urls = [
@@ -290,18 +225,10 @@ class GUI(customtkinter.CTk):
 
         ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
 
     def warm_btn(self):
         urls = [
@@ -310,25 +237,66 @@ class GUI(customtkinter.CTk):
                 "https://www.kwestiasmaku.com/przepis/medaliony-warzywne",
                 "https://www.kwestiasmaku.com/przepis/placki-z-batatow-na-"
                 "slodko"
-                ]
+        ]
         url = random.choice(urls)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-        skladniki = soup.find("div", class_="group-skladniki field-group-"
-                                            "div")
-        przygotowanie = soup.find("div", class_="field field-name-field-"
-                                                "przygotowanie field-type-text-"
-                                                "long field-label-above")
+        scraped_skladniki, scraped_przygotowanie = get_recipe_data(url)
         self.recipe_text.delete("1.0", "end")
-        scraped = ' '.join(element.get_text() for element in przygotowanie)
-        self.recipe_text.insert("1.0", scraped)
-        scraped_text = ' '.join(element.get_text() for element in skladniki)
-        self.recipe_text.insert("1.0", scraped_text)
+        self.recipe_text.insert("1.0", scraped_przygotowanie)
+        self.recipe_text.insert("1.0", scraped_skladniki)
+
+    """
+    Funkcja wysyłania maila nie działa ze względu na zablokowany przez GOOGLE 
+    dostęp mniej zaufanych aplikacji
+    """
+    def email_btn(self):
+        input_field = CTkInputDialog(text="Podaj email")
+        email = input_field.get_input()
+        print(email)
+
+        # Get the recipe data, Retrieve the text from the recipe_text widget
+        recipe_data = self.recipe_text.get("1.0", "end-1c")
+
+        # Wywołaj funkcję send_email z przekształconą wartością zmiennej email
+        self.send_email('mailbot028@gmail.com',
+                        'Mare8Czekol', email, 'Przepis',
+                        recipe_data)
+
+    @staticmethod
+    def send_email(sender_email, sender_password, recipient_email,
+                   subject, message):
+        # Utwórz wiadomość e-mail
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = recipient_email
+        msg['Subject'] = subject
+
+        # Dodaj treść wiadomości
+        msg.attach(MIMEText(message, 'plain'))
+
+        # Zapisz treść wiadomości do pliku przepis.txt
+        with open('przepis.txt', 'w') as file:
+            file.write(message)
+
+        # Dodaj plik przepis.txt jako załącznik
+        with open('przepis.txt', 'rb') as file:
+            attachment = MIMEText(file.read(), 'plain', 'utf-8')
+            attachment.add_header('Content-Disposition', 'attachment',
+                                  filename='przepis.txt')
+            msg.attach(attachment)
+
+        # Utwórz połączenie z serwerem SMTP
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+
+        # Wyślij wiadomość e-mail
+        server.send_message(msg)
+        server.quit()
 
 
 class FoodApp(GUI):
     """
-    a class that builds the app based on the design of teh class
+    a class that builds the app based on the design of the class
     """
     def build(self):
         return GUI()
